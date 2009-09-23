8c41171280943549e0f748853ed692e270377013
Sheng Peng <pengs@slac.stanford.edu>
2009-09-23 02:18:01 +0000

    Add autoSaveRestore

--- iocBoot/ioc-sys0-bd01/st.cmd
+++ iocBoot/ioc-sys0-bd01/st.cmd
@@ -4,6 +4,8 @@
 ## everywhere it appears in this file
 
 < envPaths
+# Set IOC Shell Prompt as well:
+epicsEnvSet("IOCSH_PS1","ioc-sys0-bld1>")
 
 cd ${TOP}
 
@@ -12,10 +14,43 @@ dbLoadDatabase "dbd/BLD.dbd"
 BLD_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=pengsHost")
+#dbLoadRecords("db/BLDMCast.db")
+
+## Load database for autosave/restore status pv's
+dbLoadRecords("db/save_restoreStatus.db","P=IOC:RPTC:ABCS01:")
+
+# ======================================================================
+## Configure AutoSave and Restore
+# ================================================================
+cd ${TOP}/iocBoot
+## autosave/restore settings
+save_restoreSet_status_prefix( "IOC:SYS0:BLD1:")
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+
+set_requestfile_path("${TOP}/iocBoot/${IOC}/autosave-req")
+set_savefile_path("${TOP}/iocBoot/${IOC}/autosave")
+## ==============================================
+## New method for autosave and restore
+#  Decide what is saved by specifying it in the
+#  EPICS databases
+## =============================================
+set_pass1_restoreFile("bldParams.sav")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
+
+## ===========================================================
+## Start autosave routines to save our data
+## ===========================================================
+## Handle autosave 'commands' contained in loaded databases.
+# change directory to ioc startup area
+
+cd autosave-req
+makeAutosaveFiles()
+
+epicsThreadSleep(1)
+create_monitor_set("bldParams.req",30,"")
 
 BLDMCastStart(1);
 
