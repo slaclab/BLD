ff9389e0986d0751e2d6ae7ce65eabccc93440a4
Sheng Peng <pengs@slac.stanford.edu>
2009-10-01 01:31:46 +0000

    Clean up st.cmd for autoSave, etc

--- iocBoot/ioc-sys0-bd01/st.cmd
+++ iocBoot/ioc-sys0-bd01/st.cmd
@@ -1,6 +1,5 @@
 ## Example RTEMS startup script
 
-IOC=pwd()
 cd("../..")
 
 # Load obj file
@@ -9,13 +8,11 @@ ld("bin/RTEMS-beatnik/BLD.obj")
 ## You may have to change BLD to something else
 ## everywhere it appears in this file
 
-< envPaths
+##< envPaths
 # Set IOC Shell Prompt as well:
 epicsEnvSet("IOCSH_PS1","ioc-sys0-bld1>")
 putenv ("EPICS_CA_MAX_ARRAY_BYTES=8000000")
 putenv ("EPICS_CA_SERVER_PORT=5068")
-
-cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase("dbd/BLD.dbd")
@@ -28,23 +25,22 @@ ErConfigure(0, 0, 0, 0, 1)
 ## Load record instances ##
 ###########################
 dbLoadRecords("db/IOC-SYS0-BD01.db")
-
 dbLoadRecords("db/BLDMCast.db")
-
-## Load database for autosave/restore status pv's
-dbLoadRecords("db/save_restoreStatus.db","P=IOC:SYS0:BD01:")
 
 # ======================================================================
 ## Configure AutoSave and Restore
 # ================================================================
-cd ${TOP}/iocBoot
+cd("iocBoot")
 ## autosave/restore settings
 save_restoreSet_status_prefix( "IOC:SYS0:BD01:")
 save_restoreSet_IncompleteSetsOk(1)
 save_restoreSet_DatedBackupFiles(1)
 
-set_requestfile_path("${TOP}/iocBoot/${IOC}/autosave-req")
-set_savefile_path("${TOP}/iocBoot/${IOC}/autosave")
+#cd("ioc-sys0-bd01")
+cd(pathSubstitute("%H"))
+
+set_requestfile_path("/data/autosave-req")
+set_savefile_path("/data/autosave")
 ## ==============================================
 ## New method for autosave and restore
 #  Decide what is saved by specifying it in the
@@ -52,7 +48,6 @@ set_savefile_path("${TOP}/iocBoot/${IOC}/autosave")
 ## =============================================
 set_pass1_restoreFile("bldParams.sav")
 
-cd ${TOP}/iocBoot/${IOC}
 iocInit()
 
 ## ===========================================================
@@ -61,13 +56,15 @@ iocInit()
 ## Handle autosave 'commands' contained in loaded databases.
 # change directory to ioc startup area
 
-cd autosave-req
-makeAutosaveFiles()
+cd("/data/autosave-req")
+
+## This is only good for iocShell
+##makeAutosaveFiles()
 
 epicsThreadSleep(1)
-create_monitor_set("bldParams.req",30,"")
+create_monitor_set("bldParams.req",30,0)
 
-BLDMCastStart(0, "");
+BLDMCastStart(0, 0)
 
 ## Start any sequence programs
 #seq sncxxx,"user=pengsHost"
