70326868681b0f556b9b521aa604d6a8c85860c1
Sheng Peng <pengs@slac.stanford.edu>
2009-10-02 07:36:36 +0000

    Have to ca_enable_preemptive_callback for local PV

--- BLDApp/src/catest.c
+++ BLDApp/src/catest.c
@@ -11,7 +11,7 @@ int catest(char * name)
     double	data;
     chid	mychid;
 
-    SEVCHK(ca_context_create(ca_disable_preemptive_callback),"ca_context_create");
+    SEVCHK(ca_context_create(ca_enable_preemptive_callback),"ca_context_create");
     SEVCHK(ca_create_channel(name,NULL,NULL,10,&mychid),"ca_create_channel failure");
     SEVCHK(ca_pend_io(5.0),"ca_pend_io failure");
     SEVCHK(ca_get(DBR_DOUBLE,mychid,(void *)&data),"ca_get failure");
