8a61c85fd955ca2d559ad142cbebe95c159cf17f
strauman <>
2010-03-25 15:16:36 +0000

    20100325 (Till Straumann):
    	* BLDApp/src/BLDMCast.h: Replaced 'unsigned int' and 'double' by
    	more portable epicsUInt32 / epicsFloat64.

--- BLDApp/src/BLDMCast.h
+++ BLDApp/src/BLDMCast.h
@@ -1,42 +1,49 @@
-/* $Id$ */
+/* $Id: BLDMCast.h,v 1.16 2010/03/24 00:31:26 strauman Exp $ */
 #ifndef _BLD_MCAST_H_
 #define _BLD_MCAST_H_
 
 #include <epicsTime.h>
+#include <epicsTypes.h>
+#include <epicsMutex.h>
+#include <dbScan.h>
 
 typedef struct EBEAMINFO
 {
     epicsTimeStamp	timestamp;
-    unsigned int uMBZ1;
-    unsigned int uFiducialId;
-    unsigned int uMBZ2;
+    epicsUInt32     uMBZ1;
+    epicsUInt32     uFiducialId;
+    epicsUInt32     uMBZ2;
 
     /* Xtc Section 1 */
-    unsigned int uDamage;
-    unsigned int uLogicalId; /* source 1 */
-    unsigned int uPhysicalId;/* source 2 */
-    unsigned int uDataType;  /* Contains */
-    unsigned int uExtentSize;/* Extent */
+    epicsUInt32     uDamage;
+    epicsUInt32     uLogicalId; /* source 1 */
+    epicsUInt32     uPhysicalId;/* source 2 */
+    epicsUInt32     uDataType;  /* Contains */
+    epicsUInt32     uExtentSize;/* Extent */
 
     /* Xtc Section 2 */
-    unsigned int uDamage2;
-    unsigned int uLogicalId2;
-    unsigned int uPhysicalId2;
-    unsigned int uDataType2;
-    unsigned int uExtentSize2;
+    epicsUInt32     uDamage2;
+    epicsUInt32     uLogicalId2;
+    epicsUInt32     uPhysicalId2;
+    epicsUInt32     uDataType2;
+    epicsUInt32     uExtentSize2;
 
-    unsigned int uDamageMask;
+    epicsUInt32     uDamageMask;
 
-    double	ebeamCharge;	/* in nC */
-    double	ebeamL3Energy;	/* in MeV */
-    double	ebeamLTUPosX;	/* in mm */
-    double	ebeamLTUPosY;	/* in mm */
-    double	ebeamLTUAngX;	/* in mrad */
-    double	ebeamLTUAngY;	/* in mrad */
+    epicsFloat64	ebeamCharge;	/* in nC */
+    epicsFloat64	ebeamL3Energy;	/* in MeV */
+    epicsFloat64	ebeamLTUPosX;	/* in mm */
+    epicsFloat64	ebeamLTUPosY;	/* in mm */
+    epicsFloat64	ebeamLTUAngX;	/* in mrad */
+    epicsFloat64	ebeamLTUAngY;	/* in mrad */
 
-    double	ebeamBunchLen;	/* in Amps */
+    epicsFloat64	ebeamBunchLen;	/* in Amps */
 } EBEAMINFO;
 
 #define EBEAM_INFO_ERROR 0x4000
+
+extern EBEAMINFO    ebeamInfo;
+extern epicsMutexId mutexLock;
+extern IOSCANPVT    ioscan;         /* Trigger EPICS record */
 
 #endif
