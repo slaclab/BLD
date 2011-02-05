2da252c42b4a366b29f8c2bcd5908b4d3d8f0b5e
Ernest Williams <ernesto@slac.stanford.edu>
2011-02-05 17:23:31 +0000

    add test database to help troubleshoot ca reconnection problem

--- iocBoot/ioc-b34-bd01/st.cmd
+++ iocBoot/ioc-b34-bd01/st.cmd
@@ -1,26 +1,25 @@
 #########################################
 ## So far lanIpBasic has no MC support ##
 #########################################
-#ld("lanIpBasic.obj")
-#lanIpSetup("172.27.225.21","255.255.255.192",0,0)
+ld("lanIpBasic_mve.obj")
 #lanIpDebug=0
 #padProtoDebug=0
 
 #########################################
 ## So we just use second port and BSD  ##
 #########################################
-#ld("miscUtils.obj")
-
-## Attach 2nd NIC to BSD stack:
-#ifattach( "mve2", rtems_mve_attach, 0 )
-
-## Configure 2nd NIC
-#ifconf( "mve2", "172.27.225.21", "255.255.255.192" )
 
 cd("../..")
 
 # Load obj file
 ld("bin/RTEMS-beatnik/BLD.obj")
+
+fcomUtilSetIPADDR1("-fnet")
+
+## Configure 2nd NIC using lanIpBasic
+lanIpSetup(getenv("IPADDR1"),"255.255.255.192",0,0)
+
+fcomInit(fcomUtilGethostbyname("mc-lcls-fcom"),1000)
 
 ## You may have to change BLD to something else
 ## everywhere it appears in this file
@@ -44,25 +43,54 @@ dbLoadDatabase("dbd/BLD.dbd")
 BLD_registerRecordDeviceDriver(pdbbase)
 
 # Init PMC EVR
-#ErConfigure(0, 0, 0, 0, 1)
+ErConfigure(0, 0, 0, 0, 1)
 
 ###########################
 ## Load record instances ##
 ###########################
 dbLoadRecords("db/IOC-SYS0-BD01.db")
-#dbLoadRecords("db/BLDMCast.db")
+# 5 = '2 second'
 
+# Set the BLD data records (which are now deprecated,
+# the BLDMcastWfRecv waveform should be used instead)
+# to 'Passive' to effectively disable them.
+#dbLoadRecords("db/BLDMCast.db","DIAG_SCAN=Passive, STAT_SCAN=5")
+
+# Have a BLD listener running on this IOC and fill a waveform
+# with the BLD data.
+# We scan with event 146 (beam + .5Hz)
+#
+# NOTE: There must be one of the EVR:IOC:SYS0:BD01:EVENTxyCTRL
+#       records holding the event number we use here and it
+#       must have VME interrupts (.VME field) enabled.
+#
+#       Furthermore, you cannot use any event but only 
+#       such ones for which an event record has been
+#       instantiated with MRF ER device support -- this
+#       is thanks to the great MRF software design, yeah!
+#
+# The erEvent record enables interrupts for an event
+# the interrupt handler calls scanIoRequest(lists[event]) and
+# there must be an event record registered on that list which
+# then does post_event().
+# (Well, the VME ISR firing 'event' could IMHO directly post_event(event)
+# which would be faster, simpler and more flexible)
+# 
+
+dbLoadRecords("db/BLDMCastWfRecv.db","name=IOC:SYS0:BD01:BLDWAV, scan=Event, evnt=146, rarm=2")
+
+dbLoadRecords("db/caClientTest.db", "user=CA:Client")
 # ======================================================================
 ## Configure AutoSave and Restore
 # ================================================================
-#cd("iocBoot")
+cd("iocBoot")
 ## autosave/restore settings
-#save_restoreSet_status_prefix( "IOC:B34:BD01:")
+#save_restoreSet_status_prefix( "IOC:SYS0:BD01:")
 #save_restoreSet_IncompleteSetsOk(1)
 #save_restoreSet_DatedBackupFiles(1)
 
-cd("ioc-b34-bd01")
-cd(pathSubstitute("%H"))
+#cd("ioc-sys0-bd01")
+#cd(pathSubstitute("%H"))
 
 #set_requestfile_path("/data/autosave-req")
 #set_savefile_path("/data/autosave")
