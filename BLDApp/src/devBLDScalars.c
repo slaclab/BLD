1370d30226340409048e37a1b7d7cea0aa8a1dba
rreno <rreno@slac.stanford.edu>
2020-10-09 12:50:35 -0700

    Clean up devBLDScalars and add some documentation comments

--- BLDApp/src/devBLDScalars.c
+++ BLDApp/src/devBLDScalars.c
@@ -1,8 +1,10 @@
 /*=============================================================================
 
-  Name: devBLDMCastConstants.c
+  Name: devBLDScalars.c
 
-  Abs:  BLDMCast driver for constants that are used in BLD calculations. 
+  Abs:  BLD driver for scalar constants that are used in BLD calculations. 
+
+  This follows a similar structure to the ai device support in debBLDMCastStatus
 
   Auth: Ryan Reno (rreno)
 
@@ -24,10 +26,13 @@
 
 #include "BLDMCast.h"
 
+/**
+ * Analog Out (ao) record types
+ */
 static long init_ao ( aoRecord *precord );
 static long write_ao( aoRecord *precord );
 
-struct BLD_DEV_SUP_SET
+typedef struct
 {
     long        number;
     DEVSUPFUN   report;
@@ -36,17 +41,36 @@ struct BLD_DEV_SUP_SET
     DEVSUPFUN   get_ioint_info;
     DEVSUPFUN   write;
     DEVSUPFUN   special_linconv;
+} BLD_AO_DEV_SUP;
+
+BLD_AO_DEV_SUP devAoBLD = {
+    6,
+    NULL,
+    NULL,
+    init_ao,
+    NULL,
+    write_ao,
+    NULL
 };
 
-struct BLD_DEV_SUP_SET devAoBLD = { 6, NULL, NULL, init_ao, NULL, write_ao, NULL };
 epicsExportAddress(dset, devAoBLD);
 
+/**
+ * BLDScalars
+ * This enum is used to identify which ao record is being written to.
+ * The enumeration value is stored in the record->dpvt field as a void *.
+ * This value is used in the write_ao routine to determine which action should
+ * be taken.
+ */
 typedef enum {
     BLD_SCALAR_ETAX,
 } BLDScalars;
 
-#define CHECK_PARM(PARM,VAL)\
-    if (!strncmp(precord->out.value.instio.string,(PARM),strlen((PARM)))) {\
+/**
+ * Helper macro adapted from devBLDMCastStatus.c
+ */
+#define CHECK_AO_PARAM(PARAM,VAL)\
+    if (!strncmp(precord->out.value.instio.string,(PARAM),strlen((PARAM)))) {\
         precord->dpvt=(void *)VAL;\
         return (0);\
     }
@@ -60,11 +84,15 @@ static long init_ao( aoRecord *precord )
         goto err; 
     }
 
-    /* Initialize the scalar records */
-    CHECK_PARM("ETAX",    BLD_SCALAR_ETAX)
+    /**
+     * Initialize the AO records
+     * The first argument is the string in the record OUT field ("@Name") and
+     * the second is the BLDScalars enumeration value it should match to.
+     */
+    CHECK_AO_PARAM("ETAX",    BLD_SCALAR_ETAX)
 
     /* Failure */
-    recGblRecordError(S_db_badField, (void *) precord, "devLongoutBLD init_record, bad parm");
+    recGblRecordError(S_db_badField, (void *) precord, "devAoBLD init_record, bad parm");
 err:
     precord->pact = TRUE;
     return (S_db_badField);
@@ -75,8 +103,8 @@ static long write_ao( aoRecord *precord )
     if (bldMutex)
         epicsMutexLock(bldMutex);
 
-    long type = (long) precord->dpvt;
-    switch (type)
+    long scalarId = (long) precord->dpvt;
+    switch (scalarId)
     {
     case BLD_SCALAR_ETAX:
         etaxPV = (double) precord->val;
